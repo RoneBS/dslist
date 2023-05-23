e3f6c4f52c756e94eb870c476caf586485e2142a
RoneBS <ronebs10@hotmail.com>
2023-05-23 07:00:57 -0300

    search by id added on services

--- src/main/java/com/ronesilveira/dslist/services/GameService.java
+++ src/main/java/com/ronesilveira/dslist/services/GameService.java
@@ -5,6 +5,7 @@ import com.ronesilveira.dslist.entities.Game;
 import com.ronesilveira.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,12 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
        List<Game> result = gameRepository.findAll();
        return result.stream().map(x -> new GameMinDTO(x)).toList();
